a1ad18fc82993c1ae031364b8856a76e8d1e7068
zhanlu-wm <ncu09wangming@163.com>
2020-10-26 14:36:44 +0800

    add some test code

---
+++ leetcode-algorithms/src/test/java/java8/HashTableDebug.java
@@ -0,0 +1,30 @@
+package java8;
+
+import java.util.HashMap;
+
+public class HashTableDebug {
+
+    public static void main(String[] args) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+        map.put(1, 1);
+        map.put(2, 1);
+        map.put(3, 1);
+        map.put(4, 1);
+        map.put(5, 1);
+        map.put(6, 1);
+        map.put(7, 1);
+        map.put(8, 1);
+        map.put(9, 1);
+        map.put(11, 1);
+        map.put(12, 1);
+        map.put(13, 1);
+        map.put(14, 1);
+        map.put(15, 1);
+        map.put(16, 1);
+        map.put(17, 1);
+        map.put(18, 1);
+        map.put(19, 1);
+
+    }
+
+}
